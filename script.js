d184951374d01cad5c0c5a7b2f11686ab8fc7ffb
destino92 <destinodello@gmail.com>
2016-12-05 23:31:53 +0300

    use bound data to set cx attribute of selection

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ var circle = d3.selectAll("circle");
 circle.style("fill", "tomato");
 circle.attr("r", 30);
 circle.attr("cx", function() { return Math.random() * 720; });
+circle.data([32, 57, 112]);
+
+circle.attr("r", function(d) { return Math.sqrt(d); });
